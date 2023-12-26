aa7f15473dcf530ca826dfb5158dd6ee42e5625a
Paul Gung <gongshaoxu@gmail.com>
2023-12-26 17:20:30 +0800

    img scanner

---
+++ img-scanner/README.md
@@ -0,0 +1,19 @@
+# Image Size Analyzer Plugin
+
+这是一个用于分析前端项目中图片大小的插件。
+
+## 安装
+
+在项目根目录执行以下命令安装依赖：
+
+```bash
+npm install sharp glob
+```
+
+## 使用方法
+
+在命令行中运行该脚本：
+
+```bash
+ts-node analyze-images.ts
+```
---
+++ img-scanner/src/index.ts
@@ -0,0 +1,28 @@
+import * as path from 'path';
+import * as fs from 'fs';
+import sizeOf from 'image-size';
+import { promisify } from 'util';
+
+const imgFolder = path.join(process.cwd(), "dist", "img");
+const readdirAsync = promisify(fs.readdir);
+const statAsync = promisify(fs.stat);
+const sizeOfAsync = promisify(sizeOf);
+
+async function readImages() {
+    try {
+        const files = await readdirAsync(imgFolder);
+
+        for (const file of files) {
+            const filePath = path.join(imgFolder, file);
+            const stats = await statAsync(filePath);
+            const dimensions = await sizeOfAsync(filePath);
+
+            const sizeInMB = stats.size / (1024 * 1024);
+            console.log(`${file} 尺寸: ${dimensions.width}x${dimensions.height}, 大小: ${sizeInMB.toFixed(2)} MB`);
+        }
+    } catch (err) {
+        console.error('处理图片时出错: ', err);
+    }
+}
+
+readImages();
